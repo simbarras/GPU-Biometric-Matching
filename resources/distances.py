ece1533cb4e9ab90f852e025c83c74b410c4b380
Simon Sommerhalder <simonsom@student.ethz.ch>
2022-04-25 16:51:54 +0200

    experiments up to viii

--- resources/distances.py
+++ resources/distances.py
@@ -11,14 +11,29 @@ def compute_single_distance(model, probe, distance_function):
         return compute_skeleton_hd(model, probe)
     elif distance_function == "overlap_distance":
         return compute_overlap_dist(model, probe)
+    elif distance_function == "miura_distance":
+        return compute_miura_distance(model, probe)
     elif distance_function == "always_perfect":
         return 0
     else:
         raise NotImplementedError()
 
 def compute_overlap_dist(model, probe):
-    model = shift(model, 10, 10)
-    return np.sum(np.bitwise_and(model, probe))
+    # model = shift(model, 10, 10)
+    plt.imshow(model + 2 * probe)
+    dist = np.sum(np.bitwise_and(model.astype(int), probe.astype(int)))
+    plt.suptitle(dist)
+    plt.show()
+    return dist
+
+def compute_miura_distance(model, probe):
+    aandb = np.bitwise_and(model.astype(int), probe.astype(int))
+
+    plt.imshow(model + 2 * probe)
+    dist = 1 - (np.count_nonzero(aandb) / (np.count_nonzero(model) + np.count_nonzero(probe)))
+    plt.suptitle(dist)
+    plt.show()
+    return dist
 
 def compute_skeleton_hd(a, b, min_area=30):
     axorb = np.bitwise_and(a.astype(int), b.astype(int))
