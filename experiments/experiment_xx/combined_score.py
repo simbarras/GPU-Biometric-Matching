bf57a0aa7bf09d3111b7281d39b52dafefa9b3c7
Simon Sommerhalder <simonsom@student.ethz.ch>
2022-05-09 19:52:23 +0200

    combined score

--- experiments/experiment_xx/combined_score.py
+++ experiments/experiment_xx/combined_score.py
@@ -10,6 +10,20 @@ def get_mode(dist_func):
     else:
         return "distance"
 
+def combine_function(a, b, tau):
+    # return tau * a + (1 - tau) * b
+    return min(a, b)
+
+def combine_cams(df_1, df_2, tau):
+    combined_df = pd.DataFrame(columns=["distance"], index=range(df_1.shape[0]))
+
+    for i in range(df_1.shape[0]):
+        dist1 = df_1.iloc[i]["distance"]
+        dist2 = df_2.iloc[i]["distance"]
+        comb_dist = combine_function(dist1, dist2, tau)
+        combined_df.iloc[i] = [comb_dist]
+    return combined_df
+
 params = [
     ["hamming_dist", "random_subsampling_dist", "skeleton_hd", "miura_distance"],  # distance function
     ["fingerfocus", "edge"],  # mask
@@ -25,46 +39,47 @@ impostor_df = pd.read_csv("population_ii/results.csv")
 summary = pd.DataFrame(columns=["distance_function", "mask", "prealign", "preprocess", "postprocess", "postalign", "cam", "eer"],
                        index=range(len(rows)))
 
-idx = 0
-for distance_function, mask, prealign, preprocess, postprocess, postalign in rows:
-    gen_1 = genuine_df.loc[((genuine_df["distance_function"] == distance_function)
-                         & (genuine_df["mask"] == mask))
-                         & (genuine_df["prealign"] == prealign)
-                         & (genuine_df["preprocess"] == preprocess)
-                         & (genuine_df["postprocess"] == postprocess)
-                         & (genuine_df["postalign"] == postalign)
-                         & (genuine_df["camera_m"] == 1)]
-    gen_2 = genuine_df.loc[((genuine_df["distance_function"] == distance_function)
-                         & (genuine_df["mask"] == mask))
-                         & (genuine_df["prealign"] == prealign)
-                         & (genuine_df["preprocess"] == preprocess)
-                         & (genuine_df["postprocess"] == postprocess)
-                         & (genuine_df["postalign"] == postalign)
-                         & (genuine_df["camera_m"] == 2)]
+gen_1 = genuine_df.loc[((genuine_df["distance_function"] == "miura_distance")
+                     & (genuine_df["mask"] == "edge"))
+                     & (genuine_df["prealign"] == "huang_normalization")
+                     & (genuine_df["preprocess"] == "hist_eq")
+                     & (genuine_df["postprocess"] == "skeletonize")
+                     & (genuine_df["postalign"] == "id")
+                     & (genuine_df["camera_m"] == 1)]
+gen_2 = genuine_df.loc[((genuine_df["distance_function"] == "miura_distance")
+                     & (genuine_df["mask"] == "edge"))
+                     & (genuine_df["prealign"] == "translation")
+                     & (genuine_df["preprocess"] == "hist_eq")
+                     & (genuine_df["postprocess"] == "skeletonize")
+                     & (genuine_df["postalign"] == "id")
+                     & (genuine_df["camera_m"] == 2)]
 
-    imp_1 = impostor_df.loc[((impostor_df["distance_function"] == distance_function)
-                         & (impostor_df["mask"] == mask))
-                         & (impostor_df["prealign"] == prealign)
-                         & (impostor_df["preprocess"] == preprocess)
-                         & (impostor_df["postprocess"] == postprocess)
-                         & (impostor_df["postalign"] == postalign)
-                         & (impostor_df["camera_m"] == 1)]
-    imp_2 = impostor_df.loc[((impostor_df["distance_function"] == distance_function)
-                         & (impostor_df["mask"] == mask))
-                         & (impostor_df["prealign"] == prealign)
-                         & (impostor_df["preprocess"] == preprocess)
-                         & (impostor_df["postprocess"] == postprocess)
-                         & (impostor_df["postalign"] == postalign)
-                         & (impostor_df["camera_m"] == 2)]
+imp_1 = impostor_df.loc[((impostor_df["distance_function"] == "miura_distance")
+                     & (impostor_df["mask"] == "edge"))
+                     & (impostor_df["prealign"] == "huang_normalization")
+                     & (impostor_df["preprocess"] == "hist_eq")
+                     & (impostor_df["postprocess"] == "skeletonize")
+                     & (impostor_df["postalign"] == "id")
+                     & (impostor_df["camera_m"] == 1)]
+imp_2 = impostor_df.loc[((impostor_df["distance_function"] == "miura_distance")
+                     & (impostor_df["mask"] == "edge"))
+                     & (impostor_df["prealign"] == "translation")
+                     & (impostor_df["preprocess"] == "hist_eq")
+                     & (impostor_df["postprocess"] == "skeletonize")
+                     & (impostor_df["postalign"] == "id")
+                     & (impostor_df["camera_m"] == 2)]
 
-    gen = gen_1["distance"]  + gen_2["distance"] * 2
-    imp = imp_1["distance"] + imp_2["distance"] * 2
-    eer, tpr, fpr = get_eer_confusion(gen, imp, get_mode(distance_function))
-    #plt.suptitle(distance_function + " " + mask + " " + prealign + " " + preprocess + " " + postprocess + " " + postalign + " " + str(camera))
-    #show_roc([tpr], [fpr], ["eer: " +  str(round(eer, 3))])
-    summary.iloc[idx] = [distance_function, mask, prealign, preprocess, postprocess, postalign, eer]
-    print(eer)
-    idx += 1
+best_tau = 1
+best_eer = 1
 
-print(summary)
-summary.to_csv("combined_summary.csv")
+for t in range(21):
+    tau = (20 - t) / 20
+    gen_comb = combine_cams(gen_1, gen_2, tau)
+    imp_comb = combine_cams(imp_1, imp_2, tau)
+    eer, tpr, fpr = get_eer_confusion(gen_comb, imp_comb, get_mode("miura_distance"))
+    if eer < best_eer:
+        best_tau = tau
+        best_eer = eer
+# plt.suptitle(distance_function + " " + mask + " " + prealign + " " + preprocess + " " + postprocess + " " + postalign + " " + str(camera))
+# show_roc([tpr], [fpr], ["eer: " +  str(round(eer, 3))])
+print(best_eer, best_tau)
