673c7824949104fa4340069fea431d1966ce5a96
Lara Lenz <lara.lenz@epfl.ch>
2024-02-29 12:51:34 +0100

    Add test file of which everything will be based

---
+++ alignment-cpp/pipeline.cpp
@@ -0,0 +1,12 @@
+#include "NumCpp.hpp"
+#include "opencv2/opencv.hpp" 
+#include <Eigen/Dense>
+#include <iostream> //all of these are for testing purposes right now
+
+using namespace std;
+
+int main() {
+    // here should be the code to be added
+    cout << "Check if this is working" << endl;
+    return 0;
+}
