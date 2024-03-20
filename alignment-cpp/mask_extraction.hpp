e54b6c83e5a761faf378db23dbbae212fc8367c7
Lara Lenz <lara.lenz@epfl.ch>
2024-03-20 11:18:49 +0100

    Mask Extraction: Complete mask extraction and start adding documentation (small code optimizations from python source)

--- alignment-cpp/mask_extraction.hpp
+++ alignment-cpp/mask_extraction.hpp
@@ -1,13 +1,31 @@
 #include <NumCpp.hpp>
+#include <opencv2/core/mat.hpp>
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <Eigen/Dense>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
 #include <iostream>
 #include <array>
 #include <numeric>
 #include <tuple>
 #include <list>
 
+/**
+ * This function goes over an array of values (starting from index start)
+ * going in the direction of dir and finds the maximum value that is below
+ * threshold.
+ * 
+ * @param[in] arr: A 1-dimensional NdArray of doubles that needs to be traversed.
+ * @param[in] start: The integer index value from which to start traversing.
+ * @param[in] dir: A boolean indicating the direction in which to traverse, 
+ * 'True' means traversing upwards, 'False' means traversing downwards.
+ * @param[in] threshold: This value indicates when to stop traversing, namely,
+ * if an array value is found that exceeds threshold we stop traversing.
+ * @returns The index pointing to the maximum element in the NdArray. This 
+ * element is either the maximum element of all traversed elements if all 
+ * values were below the threshold, or it refers to the first encountered 
+ * value that is above the threshold.
+*/
 int max_thresh(nc::NdArray<double> arr, int start, bool dir, int threshold) {
 
     double val = 0;
@@ -16,7 +34,7 @@ int max_thresh(nc::NdArray<double> arr, int start, bool dir, int threshold) {
     double max_val = 0;
     int max_idx = start;
 
-    // dir = false means "down", dir = true means "up"
+    // dir = False means "down", dir = True means "up"
     if (dir) {
         while (val < threshold && idx > 30) {
             val = arr[idx];
@@ -25,7 +43,8 @@ int max_thresh(nc::NdArray<double> arr, int start, bool dir, int threshold) {
                 max_idx = idx;
             }
             idx--;
-            if (prev_val >= threshold && val >prev_val) {
+            // since prev_val is never set, this is redundant
+            if (prev_val >= threshold && val > prev_val) {
                 idx++;
                 break;
             }
@@ -38,6 +57,7 @@ int max_thresh(nc::NdArray<double> arr, int start, bool dir, int threshold) {
                 max_idx = idx;
             }
             idx++;
+            // since prev_val is never set, this is redundant
             if (prev_val >= threshold && val < prev_val) {
                 idx--;
                 break;
@@ -45,30 +65,46 @@ int max_thresh(nc::NdArray<double> arr, int start, bool dir, int threshold) {
         }
     }
 
+    // if no value exceeded the threshold, return the index of the maximum value
     if (idx == 220 or idx == 30) return max_idx;
-
+    // otherwise return the index of the first value that exceed the threshold
     return idx;
 }
 
-std::array<std::array<int, 2>, 2> edge_points(nc::NdArray<double> img, int x_1, int f_1, int width, int height, int threshold = 4) {
+/**
+ * This function tries to find the edge points of a 2-dimensional NdArray of 
+ * doubles. It will take the x_1-ith column and find two points that will 
+ * denote the edges for this column.
+ * 
+ * @param[in] img: A 2-dimensional NdArray containing double values.
+ * @param[in] x_1: Denotes the index of the column from which we want to find 
+ * the edge points.
+ * @param[in] f_1: Denotes the index from which to start searching for the edge 
+ * points. (default = 130)
+ * @param[in] threshold: The value that is given to max_thresh for computation. 
+ * (default = 4)
+ * @returns An integer array of size 3 of the form 
+ * (column_index, upper_edge_point, lower_edge_point) denoting the two edge 
+ * points of the chosen column.
+*/
+std::array<int, 3> edge_points(nc::NdArray<double> img, int x_1, int f_1 = 130, int threshold = 4) {
 
-    //img[:, x_1:x_1 +1] uses the x_1-ith column of the image => have a closer look at whether it should be Rslice or Cslice
+    // Obtain the x_1-ith column of img
     nc::Slice img_slicer = img.rSlice(0, 1);
     nc::NdArray<double> img_sliced = img(img_slicer, x_1);
 
-    // Check if correct slice was taken ;)
-    //img_sliced.nc::NdArray<double>::print();
-
+    // Divide all elements of the column with the average computed over all
+    // these elements
     nc::NdArray<double> avg_1 = img_sliced.nc::NdArray<double>::sum(nc::Axis::COL);
     nc::NdArray<double> avg_comp = nc::average(avg_1);
     if (avg_comp.nc::NdArray<double>::size() != 1) abort();
     double avg = avg_comp[0];
     avg_1 = nc::operator/(avg_1, avg);
-    //we'll see whether that works
 
+    // Call max_thresh and obtain the edge points
     int a = max_thresh(avg_1, f_1, true, threshold);
     int b = max_thresh(avg_1, f_1, false, threshold);
-    std::array<std::array<int, 2>, 2>  res = {{{x_1, a}, {x_1, b}}};
+    std::array<int, 3>  res = {{x_1, a, b}};
 
     return res;
 }
@@ -90,43 +126,71 @@ std::tuple<nc::NdArray<uint8_t>, nc::NdArray<uint8_t>> edge_mask_extraction(cons
 
     nc::NdArray<double> gradient = nc::hypot(gradient_x, gradient_y);
 
-
-    int mid_y = 130;
-
-    std::list<int> points_up_x;
+    std::list<int> points_up_down_x;
     std::list<int> points_up_y;
-    std::list<int> points_down_x;
     std::list<int> points_down_y;
 
     int start = std::get<0>(roi);
     int end = std::get<1>(roi);
 
     for (int i = start; i < end; i++) {
-        std::array<std::array<int, 2>, 2> ps = edge_points(gradient, i, mid_y, width, height);
-        points_up_x.push_back(ps[0][0]);
-        points_up_y.push_back(ps[0][1]);
-        points_down_x.push_back(ps[1][0]);
-        points_down_y.push_back(ps[1][1]);
+        std::array<int, 3> ps = edge_points(gradient, i);
+        points_up_down_x.push_back(ps[0]);
+        points_up_y.push_back(ps[1]);
+        points_down_y.push_back(ps[2]);
     }
 
     nc::NdArray<uint8_t> mask = nc::zeros_like<uint8_t>(img);
 
-    for (; !points_up_x.empty();) {
-        int ux = points_up_x.front();
-        points_up_x.pop_front();
+    for (; !points_up_down_x.empty();) {
+        int ux = points_up_down_x.front();
+        points_up_down_x.pop_front();
         int uy = points_up_y.front();
         points_up_y.pop_front();
         int dy = points_down_y.front();
         points_down_y.pop_front();
 
         for (int i = uy; i < dy; i++) {
-            if (img[i*width + ux] <= 240) {
-                mask[i*width + ux] = 1;
-                std::cout << "A one was set" << i << ", " << ux << std::endl;
+            if (img(i, ux) <= 240) {
+                mask(i, ux) = 1;
             }
         }
     }
 
+    int morph_size = 1;
 
-    return {img, img};
+    
+    cv::Mat maskOCV(height, width, CV_8U, &(mask(0,0)));
+    cv::Mat dest = cv::Mat::zeros(height, width, CV_8U);
+    /** structure used for closing in python (use MORPH_CROSS)
+     * [[False  True False]
+       [ True  True  True]
+       [False  True False]]
+
+    */
+    cv::Mat kernel = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3), cv::Point(1, 1));
+    cv::morphologyEx(maskOCV, dest, cv::MORPH_CLOSE, kernel);
+
+    cv::Mat kernel2 = cv::Mat::zeros(3, 5, CV_8U);
+    for (int i = 0; i < 5; i++) {
+        kernel2.at<uint8_t>(cv::Point(i, 1)) = 1;
+    }
+    
+    cv::morphologyEx(dest, maskOCV, cv::MORPH_OPEN, kernel2, cv::Point(-1, -1), 10); 
+
+    // if I understood correctly then in order to compute the convex hull, we
+    // first need to find the contours
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours( maskOCV, contours, cv::RETR_TREE, cv::CHAIN_APPROX_NONE);
+
+    if (contours.size() != 1) abort();
+    //compute convex hull
+    std::vector<std::vector<cv::Point>> hull(contours.size());
+    for( size_t i = 0; i < contours.size(); i++) {
+        cv::convexHull( contours[i], hull[i] );
+    }
+
+    cv::fillConvexPoly(maskOCV, hull[0], cv::Scalar_<uint8_t>(1));
+
+    return {img, mask};
 }
