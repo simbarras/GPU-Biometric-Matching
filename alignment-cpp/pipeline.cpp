c9b0b9140f56f36bd44531d221ec4cb8492f3b43
Lara Lenz <lara.lenz@epfl.ch>
2024-04-01 15:05:32 +0200

    Pipeline: Remove default values from .cpp

--- alignment-cpp/pipeline.cpp
+++ alignment-cpp/pipeline.cpp
@@ -80,12 +80,12 @@ nc::NdArray<uint8_t> readpng_file_to_array(const char* filename, const int wid, 
 
 nc::NdArray<bool> run_pipeline(const int width, const int height, 
                                int camera_persp, 
-                               nc::NdArray<uint8_t>* image = nullptr, 
-                               const char* image_path = nullptr, 
-                               const nc::NdArray<bool>* modelIn = nullptr, 
-                               const char* model_path = nullptr, 
-                               bool caching = false, 
-                               std::string cache_path = "") {
+                               nc::NdArray<uint8_t>* image, 
+                               const char* image_path, 
+                               const nc::NdArray<bool>* modelIn, 
+                               const char* model_path, 
+                               bool caching, 
+                               std::string cache_path) {
 
     assert(image != nullptr || image_path != nullptr);
 
