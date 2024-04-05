7d949962d263b3a3ac236c2824d228f50456b1a2
Lara Lenz <lara.lenz@epfl.ch>
2024-04-05 11:57:27 +0200

    Move png read-in into main and out of pipeline, change input parameter and implementation accordingly

--- alignment-cpp/pipeline.cpp
+++ alignment-cpp/pipeline.cpp
@@ -1,89 +1,16 @@
 #include "pipeline.hpp"
 
-nc::NdArray<uint8_t> readpng_file_to_array(std::string* filename, const int wid, const int hei) {
-
-    // Open the file and abort if there is an error
-    FILE *fp = fopen(filename->c_str(), "rb");
-    if (!fp) abort();
-
-    // Create structures needed for reading the PNG
-    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-    if(!png) abort();
-
-    png_infop info = png_create_info_struct(png);
-    if(!info) abort();
-
-    // libpng "error code" handling
-    if(setjmp(png_jmpbuf(png))) abort();
-
-    // Take filestream pointer and store in png
-    png_init_io(png, fp);
-    png_read_info(png, info);
-
-    int width; 
-    int height; 
-    png_byte color_type; 
-    png_byte bit_depth; 
-    uint8_t** row_pointers = nullptr;
-
-    width      = png_get_image_width(png, info);
-    height     = png_get_image_height(png, info);
-    color_type = png_get_color_type(png, info);
-    bit_depth  = png_get_bit_depth(png, info);
-
-    // if width or height do not equal the expected width and height, abort
-    if (width != wid || height != hei) abort();
-
-    // if row pointers already set, abort
-    if (row_pointers) abort();
-
-    // Allocate space for row_pointers
-    row_pointers = (uint8_t**)malloc(sizeof(uint8_t*) * height);
-    for(int y = 0; y < height; y++) {
-        row_pointers[y] = (uint8_t*)malloc(png_get_rowbytes(png,info));
-    }
-    // Read out the images information
-    png_read_image(png, row_pointers);
-    // Close the filestream and destroy the read structure
-    fclose(fp);
-    png_destroy_read_struct(&png, &info, nullptr);
-
-    //turn the row_pointers structure into an NdArray
-    std::array<std::array<uint8_t, 376>, 240> img_arr;
-
-    for(int y = 0; y < height; y++) {
-        png_bytep row = row_pointers[y];
-        for(int x = 0; x < width; x++) {
-            img_arr[y][x] = row[x];
-        }
-        free(row);
-    }
-
-    free(row_pointers);
-
-    nc::NdArray<uint8_t> img = nc::NdArray<uint8_t>(img_arr);
-
-    return img;
-}
-
 nc::NdArray<bool> run_pipeline(const int width, const int height, 
                                int camera_persp, 
-                               nc::NdArray<uint8_t>* image, 
-                               std::string* image_path, 
-                               const nc::NdArray<bool>* modelIn, 
-                               std::string* model_path, 
-                               bool caching, 
-                               std::string* cache_path) {
+                               nc::NdArray<uint8_t>* image,
+                               const nc::NdArray<bool>* modelIn,
+                               bool saveIntermediateSteps, 
+                               std::string* saveIntermediateSteps_path) {
     
-    assert(image != nullptr || image_path != nullptr);
+    assert(image != nullptr);
 
     // Open and load the image to use
-    nc::NdArray<uint8_t> img;
-    if (image != nullptr) {
-        img = *image;
-    } else if (image_path != nullptr) {
-        img = readpng_file_to_array(image_path, width, height); 
-    }
+    nc::NdArray<uint8_t> img = *image;
 
     // Extract mask
     nc::NdArray<uint8_t> mask;
@@ -110,13 +37,8 @@ nc::NdArray<bool> run_pipeline(const int width, const int height,
     // TODO: Add "caching" of images
 
     // Postalignment with the help of a model
-    if (model_path != nullptr || modelIn != nullptr) {
-        nc::NdArray<bool> model;
-        if (modelIn != nullptr) {
-            model = *modelIn;
-        } else if (model_path != nullptr) {
-            model = readpng_file_to_array(model_path, width, height).astype<bool>();
-        }
+    if (modelIn != nullptr) {
+        nc::NdArray<bool> model = *modelIn;
         veins = miura_matching(veins, model, width, height);
     } 
 
--- alignment-cpp/pipeline.hpp
+++ alignment-cpp/pipeline.hpp
@@ -1,7 +1,5 @@
 #include "NumCpp.hpp"
-#include "png.h"
 #include <iostream>
-#include <array>
 #include <tuple>
 
 #include "mask_extraction.hpp"
@@ -12,18 +10,6 @@
 
 #ifndef PIPELINE_H
 #define PIPELINE_H
-
-/**
- * This function reads out a 8-bit grayscale png file and writes it into a NdArray.
- * 
- * @param[in] filename: The file path as a string that leads to the image that
- * will run through the pipeline. It needs to be constant.
- * @param[in] wid: The expected width of the image given as a constant integer.
- * @param[in] hei: The expected height of the image given as a constant integer.
- * @returns A NumCpp NdArray of type uint8_t that contains the 8-bit grayscale 
- * pixel values of the image. 
-*/
-nc::NdArray<uint8_t> readpng_file_to_array(const char* filename, const int wid, const int hei);
 
 /**
  * This function executes the entire pipeline. The steps are as follows:
@@ -40,28 +26,20 @@ nc::NdArray<uint8_t> readpng_file_to_array(const char* filename, const int wid, 
  * @param[in] camera_persp: An integer denoting which camera the image was
  * provided by (either 1 or 2).
  * @param[in] image: A 2-dimensional NdArray of uint8_t containing the image 
- * that will be run through the pipeline. Either image or image_path need to 
- * be defined. (default = nullptr) 
- * @param[in] image_path: The file path as a string that leads to the image that
- * will run through the pipeline. It needs to be constant. Either image or 
- * image_path need to be defined. (default = nullptr)
+ * that will be run through the pipeline. This needs to be defined.
  * @param[in] modelIn: A 2-dimensional NdArray of bool containing a model that 
  * can be used as reference for the input. (default = nullptr)
- * @param[in] model_path: The file path as a string that leads to the model that
- * will be used as reference. (default = nullptr)
- * @param[in] caching: A boolean indicating whether the result of each pipeline
+ * @param[in] saveIntermediateSteps: A boolean indicating whether the result of each pipeline
  * step should be saved. (default = false)
- * @param[in] cache_path: The file path as a string where the caching results
+ * @param[in] saveIntermediateSteps_path: The file path as a string pointer where the caching results
    should be stored. (default = nullptr)
  * @returns An extracted and aligned feature vector.
 */
 nc::NdArray<bool> run_pipeline(const int width, const int height, 
                                int camera_persp, 
-                               nc::NdArray<uint8_t>* image = nullptr, 
-                               std::string* image_path = nullptr, 
-                               const nc::NdArray<bool>* modelIn = nullptr, 
-                               std::string* model_path = nullptr, 
-                               bool caching = false, 
-                               std::string* cache_path = nullptr);
+                               nc::NdArray<uint8_t>* image,
+                               const nc::NdArray<bool>* modelIn = nullptr,
+                               bool saveIntermediateSteps = false, 
+                               std::string* saveIntermediateSteps_path = nullptr);
 
 #endif
