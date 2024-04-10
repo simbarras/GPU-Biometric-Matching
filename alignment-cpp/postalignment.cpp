0a15338938630ef81b823c576868ba4fc02413b7
Lara Lenz <lara.lenz@epfl.ch>
2024-04-10 18:12:04 +0200

    Postalignment: Add documentation

--- alignment-cpp/postalignment.cpp
+++ alignment-cpp/postalignment.cpp
@@ -1,6 +1,5 @@
 #include "postalignment.hpp"
 
-
 std::tuple<int, int> unravel_index (int arg_max, int width, int height) {
     int y = arg_max % width;
     int x = arg_max / width;
@@ -9,24 +8,20 @@ std::tuple<int, int> unravel_index (int arg_max, int width, int height) {
 }
 
 std::tuple<double, int, int> miura_score (nc::NdArray<bool> model,
-                                                nc::NdArray<bool> probe,
-                                                int width,
-                                                int height,
-                                                int ch,
-                                                int cw) {
-    nc::Shape Rsh = model.shape();
-    int h = Rsh.rows;
-    int w = Rsh.cols;
+                                          nc::NdArray<bool> probe,
+                                          int width,
+                                          int height,
+                                          int ch,
+                                          int cw) {
 
-    nc::Slice rSlice = nc::Slice(ch, h - ch);
-    nc::Slice cSlice = nc::Slice(cw, w - cw);
+    // Slices the model and uses it as a kernel for the convolution
+    nc::Slice rSlice = nc::Slice(ch, height - ch);
+    nc::Slice cSlice = nc::Slice(cw, width - cw);
 
     nc::NdArray<bool> crop_model = model(rSlice, cSlice);
     nc::Shape cSh = crop_model.shape();
     int hei = cSh.rows;
     int wid = cSh.cols;
-
-    nc::Slice slice = nc::Slice(0, wid);
 
     // Now, in the reference implementation they use fftconvolve on a rotated
     //matrix, however, using the rotated matrix directly for convolution will
@@ -34,31 +29,41 @@ std::tuple<double, int, int> miura_score (nc::NdArray<bool> model,
     //which yields equivalent results to the reference. 
 
     //crop_model = nc::rot90(crop_model, 2);
+
+    // Makes both input matrices to double for convolution
     nc::NdArray<double> in1 = probe.astype<double>();
     nc::NdArray<double> in2 = crop_model.astype<double>();
 
     cv::Mat probeOCV(height, width, CV_64F, &(in1(0,0)));
     cv::Mat kernelOCV(hei, wid, CV_64F, &(in2(0,0)));
 
+    // Generates an output matrix for the convolution
     nc::NdArray<double> N_c = nc::zeros<double>(height, width);
     cv::Mat NcOCV(height, width, CV_64F, &(N_c(0,0)));
 
+    // Convolves the probe and the cropped model
     cv::filter2D(probeOCV, NcOCV, -1, kernelOCV, cv::Point(-1, -1), (0.0), cv::BORDER_REPLICATE);
 
-    // TODO: Find out where exactly to take the slice from since there is no 1:1
-    // matching with fftconvolve
+    // Now this must look like Slicing magic, and it kind of is. The reference
+    // implementation makes use of fftconvolve in 'valid' mode which crops the
+    // output image to the values that do not rely on the zero padding. Since 
+    // there is no equivalent operation for OpenCV, I had to compute the 
+    // 'center' by the definitions of SciPy and from the output that we obatin.
     N_c = N_c(nc::Slice(90, 151), nc::Slice(98, 279));
     nc::Shape NcSh = N_c.shape();
     int heightNc = NcSh.rows;
     int widthNc = NcSh.cols;
 
+    // Finds the maximum value of the output array (the index where the images
+    // were maximally aligned)
     std::tuple<int, int> unrav = unravel_index(N_c.argmax()(0, 0), widthNc, heightNc);
     
     int t0 = std::get<0>(unrav);
     int s0 = std::get<1>(unrav);
     
-    nc::Slice rSlice2 = nc::Slice(t0, t0 + h - 2 * ch);
-    nc::Slice cSlice2 = nc::Slice(s0, s0 + w - 2 * cw);
+    // Computes a score denoting how well the images could be aligned
+    nc::Slice rSlice2 = nc::Slice(t0, t0 + height - 2 * ch);
+    nc::Slice cSlice2 = nc::Slice(s0, s0 + width - 2 * cw);
     double R_c = N_c(t0, s0) / ((nc::count_nonzero(crop_model)(0, 0)) + (nc::count_nonzero(probe(rSlice2, cSlice2))(0, 0)));
 
     return {R_c, t0, s0};
@@ -66,14 +71,18 @@ std::tuple<double, int, int> miura_score (nc::NdArray<bool> model,
 
 nc::NdArray<bool> miura_matching (nc::NdArray<bool> image, const nc::NdArray<bool> model, int width, int height) {
 
+    // The default values where the convolution would be maximal, in case, that
+    // probe and model are equivalent
     int ch = 30;
     int cw = 90;
 
+    // Computes the miura score, and the indices by which to shift
     std::tuple<double, int, int> res = miura_score(model, image, width, height);
     double score = std::get<0>(res);
     int t0 = std::get<1>(res);
     int s0 = std::get<2>(res);
 
+    // Shifts the probe matrix such that probe and model are maximally aligned
     image = shiftMat(image, t0 - ch, s0 - cw, width, height);
 
     return image;
--- alignment-cpp/postalignment.hpp
+++ alignment-cpp/postalignment.hpp
@@ -8,9 +8,47 @@
 #ifndef POSTALIGNMENT_H
 #define POSTALIGNMENT_H
 
-
+/**
+ * This function is equivalent to unravel_index by NumPy. It takes as input 
+ * an integer in the range of a flattened (height x width) array, and 
+ * returns the indices of the unflattened array. 
+ * 
+ * @param[in] arg_max: An integer denoting an index of a flattened array. 
+ * This integer should be in the range of (0, (height * width) - 1).
+ * @param[in] width: An integer denoting the width (#columns) of the 
+ * unflattened array.
+ * @param[in] height: An integer denoting the height (#rows) of the 
+ * unflattened array.
+ * @returns A tuple denoting the indices ((x, y)-values) of an unflattened 
+ * array.
+*/
 std::tuple<int, int> unravel_index (int arg_max, int width, int height);
 
+/**
+ * This function computes a score denoting how well model and probe can be
+ * maximally aligned. Furthermore, two integers are given denoting by how much
+ * the probe should be shifted such that this maximal score can be achieved.
+ * 
+ * @param[in] model: A 2-dimensional NdArray of bools containing the 
+ * fingerveins of the model.
+ * @param[in] probe: A 2-dimensional NdArray of bools containing the 
+ * fingerveins of the probe.
+ * @param[in] width: An integer denoting the width (#columns) of the 
+ * input images.
+ * @param[in] height: An integer denoting the height (#rows) of the 
+ * input images.
+ * @param[in] ch: An integer denoting by how much the model should be 
+ * cropped in the first dimension (height) in order to use it as a 
+ * kernel for convolution. (default = 30)
+ * @param[in] cw: An integer denoting by how much the model should be 
+ * cropped in the second dimension (width) in order to use it as a 
+ * kernel for convolution. (default = 90)
+ * @returns A tuple consisting of a double and two integers. The double denotes 
+ * a score indicating how well model and probe can be maximally aligned. The 
+ * integers represent by how much the probe should be shifted to be maximally 
+ * aligned with the probe. The first integer is used for the first dimension 
+ * (height), the second for the second dimension (width).
+*/
 std::tuple<double, int, int> miura_score (nc::NdArray<bool> model,
                                                 nc::NdArray<bool> probe,
                                                 int width,
@@ -18,6 +56,21 @@ std::tuple<double, int, int> miura_score (nc::NdArray<bool> model,
                                                 int ch = 30,
                                                 int cw = 90);
 
+/**
+ * This function shifts the input image with respect to a model image, 
+ * such that both of them are maximally aligned.
+ * 
+ * @param[in] image: A 2-dimensional NdArray of bools containing the 
+ * fingervein image of the probe.
+ * @param[in] model: A 2-dimensional NdArray of bools containing the 
+ * fingervein image of the model.
+ * @param[in] width: An integer denoting the width (#columns) of the 
+ * input images.
+ * @param[in] height: An integer denoting the height (#rows) of the 
+ * input images.
+ * @returns A 2-dimensional NdArray of bools containing the shifted 
+ * input image, so that the model and probe are maximally aligned.
+*/
 nc::NdArray<bool> miura_matching (nc::NdArray<bool> image, const nc::NdArray<bool> model, int width, int height);
 
 #endif
