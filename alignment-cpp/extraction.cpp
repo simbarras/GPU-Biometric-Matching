d3415c82115d37c0fad43a887b8ee69c23697ce9
Lara Lenz <lara.lenz@epfl.ch>
2024-04-10 17:22:52 +0200

    Extraction: Move documentation to header file and update documentation according to current version

--- alignment-cpp/extraction.cpp
+++ alignment-cpp/extraction.cpp
@@ -1,23 +1,5 @@
 #include "extraction.hpp"
 
-/**
- * This function detects valleys in the image respecting the mask.
- * 
- * @param[in] image: A 2-dimensional NdArray of doubles containing 
- * the input image.
- * @param[in] mask: A 2-dimensional NdArray of doubles of the same 
- * size as input indicating where the finger can be found in the image.
- * @param[in] sigma: A double denoting the varianbce of the Gaussian 
- * filter.
- * @param[in] width: An integer denoting the width (#columns) of 
- * the image and mask.
- * @param[in] height: An integer denoting the height (#rows) of 
- * the image and mask.
- * @returns A vector of four 2-dimensional NdArrays of doubles. Each 
- * array denotes the cross-section valley detection for a specific 
- * direction. The considered direction are horizontal, vertical, 45°, 
- * and -45° (in this order).
-*/
 std::vector<nc::NdArray<double>> detect_valleys (nc::NdArray<double> image,
                                     nc::NdArray<double> mask,
                                     double sigma,
@@ -101,16 +83,6 @@ std::vector<nc::NdArray<double>> detect_valleys (nc::NdArray<double> image,
 
 }
 
-/**
- * This function finds vein probabilities in a 1-dimensional array.
- * 
- * @param[in] a: A 1-dimensional NdArray of doubles for which we 
- * want to find the vein probabilities.
- * @param[in] width: An integer denoting the size (number of columns) 
- * of a.
- * @returns A 1-dimensional NdArray of doubles denoting the vein 
- * center probabilities.
-*/
 nc::NdArray<double> _prob_1d (nc::NdArray<double> a, int width) {
 
     nc::NdArray<double> z = nc::zeros_like<double>(a);
@@ -150,17 +122,6 @@ nc::NdArray<double> _prob_1d (nc::NdArray<double> a, int width) {
     return z;
 }
 
-/**
- * This function finds all the indices of the n-th diagonal from a 
- * matrix of size (heigth x width).
- * 
- * @param[in] nth_diag: An integer denoting the n-th diagonal for 
- * which we want to find all indices.
- * @param[in] width: An integer denoting the width (#columns) of 
- * the matrix.
- * @param[in] height: An integer denoting the heigth (#rows) of 
- * the matrix.
-*/
 std::vector<std::tuple<int, int>> diag_indices(int nth_diag, int width, int height) {
     assert(nth_diag < width && nth_diag > -height);
 
@@ -175,30 +136,6 @@ std::vector<std::tuple<int, int>> diag_indices(int nth_diag, int width, int heig
     return res;
 }
 
-/**
- * This function evaluates the joint vein center probabilities taking 
- * valley widths and depths into consideration. 
- * 
- * The following explanation is taken from the reference implementation:
- * Once the arrays of curvatures (concavities) are calculated, detection 
- * works as follows: The code scans the image in a precise direction 
- * (vertical, horizontal, diagonal, etc). It tries to find a concavity 
- * in each direction and measures its width. It then identifies the 
- * centers of the concavity and assigns a value to it, which depends on 
- * its width and maximum depth (where the peak of darkness occurs) in 
- * such a concavity. This value is accumulated on a variable, which is 
- * re-used for all directions. This variable represents the vein 
- * probabilites.
- * 
- * @param[in] input_matrices: A vector consisting of 4 2-dimensional 
- * NdArrays of doubles, each array denotes cross-section valley 
- * detections for each of the contemplated directions (horizontal, 
- * vertical, 45°, and -45°; in this order).
- * @param[in] width: An integer denoting the width (#columns) of each array.
- * @param[in] height: An integer denoting the height (#rows) of each array.
- * @returns A 2-dimensional NdArray of doubles denoting the 
- * un-accumulated vein center probabilities.
-*/
 nc::NdArray<double> eval_vein_probabilities (std::vector<nc::NdArray<double>> input_matrices,
                                              int width,
                                              int height) {
@@ -263,17 +200,6 @@ nc::NdArray<double> eval_vein_probabilities (std::vector<nc::NdArray<double>> in
     return V;
 }
 
-/**
- * This function connects the centers in the given 1-dimensional array.
- * 
- * @param[in] a: A 1-dimensional NdArray of doubles for which we want 
- * to connect the centers.
- * @param[in] width: An integer denoting the size (number of columns) 
- * of a.
- * @returns A 1-dimensional NdArray of doubles containing the corrected 
- * pixel values after filtering. Note that the output array is 4 elements 
- * shorter than the input array due to the windowing operation.
-*/
 nc::NdArray<double> _connect_1d (nc::NdArray<double> a, int width) {
     nc::NdArray<double> z = nc::zeros<double>(1, 0);
     if (width - 4 < 1) return z;
@@ -288,18 +214,6 @@ nc::NdArray<double> _connect_1d (nc::NdArray<double> a, int width) {
     return nc::amin(nc::stack({max1, max2}, nc::Axis::ROW), nc::Axis::ROW);
 }
 
-/**
- * This function connects vein centers by filtering vein probabilities.
- * 
- * @param[in] V: A 2-dimensional NdArray of doubles which represent the 
- * accumulated vein center probabilities.
- * @param[in] width: An integer denoting the number of columns of V.
- * @param[in] height: An integer denoting the number of rows of V.
- * @returns A vectors consisting of four 2-dimensional NdArrays of 
- * doubles. These arrays contain the results of the filtering operation 
- * for each of the directions. Each array corresponds to the horizontal, 
- * vertical, +45° (/), and -45° (\) directions.
-*/
 std::vector<nc::NdArray<double>> connect_centers (nc::NdArray<double> V, int width, int height) {
 
     // Initializes the "3"-dimensional matrix Cd which we implement as a vector
@@ -373,15 +287,6 @@ std::vector<nc::NdArray<double>> connect_centers (nc::NdArray<double> V, int wid
     return Cd;
 }
 
-/**
- * This function binarises vein images by using a threshold. 
- * This works under the assumption that the distribution is disphasic.
- * 
- * @param[in] G: The vein image given as a 2-dimensional NdArray of doubles
- * which we wish to binarise.
- * @returns A 2-dimensional NdArray of doubles (values are either 0 or 1) 
- * denoting where fingerveins are present in the image.
-*/
 nc::NdArray<bool> binarise (nc::NdArray<double> G) {
 
     // Produces a flattened NdArray of doubles containing all values from G that
@@ -407,28 +312,6 @@ nc::NdArray<bool> binarise (nc::NdArray<double> G) {
     return Gbool;
 }
 
-/**
- * This function extracts the fingerveins of an image given as a
- * NdArray<uint8_t>.
- * 
- * @param[in] image: The image given as a 2-dimensional 
- * NdArray of uint8_t values from which we want to extract the 
- * fingerveins.
- * @param[in] mask: A mask given as a 2-dimensional NdArray of doubles 
- * denoting the region where the finger is in the input image.
- * @param[in] width: An integer denoting the width (#columns) of 
- * the image and mask.
- * @param[in] height: An integer denoting the height (#rows) of 
- * the image and mask.
- * @param[in] sigma: A parameter used for valley detection. 
- * (default = 3)
- * @returns A tuple consisting of two 2-dimensional NdArrays of 
- * doubles of the same size as the input arrays. The first array 
- * indicates where fingerveins were found. The presence of a 
- * fingervein at a specific pixel is denoted by a 1, and 0 otherwise. 
- * The second array contains the mask which is equivalent to the 
- * input mask. 
-*/
 nc::NdArray<bool> maximum_curvature (nc::NdArray<uint8_t> image,
                                      nc::NdArray<double> mask,
                                      int width,
