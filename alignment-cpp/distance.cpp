6ba3ca7892720ee7cd96180b5de45550c4e7984f
Lara Lenz <lara.lenz@epfl.ch>
2024-04-19 11:01:51 +0200

    Compute_miura_distance: Add documentation

--- alignment-cpp/distance.cpp
+++ alignment-cpp/distance.cpp
@@ -2,10 +2,15 @@
 
 double compute_miura_distance (nc::NdArray<bool> model, nc::NdArray<bool> probe) {
 
+    // Bitwise Ands the model and probe
     nc::NdArray<bool> mAndp = model & probe;
+
+    // Counts the number of 1's in the respective images
     double countMP = (static_cast<double>(nc::count_nonzero(mAndp)(0,0)));
     double countM = (static_cast<double>(nc::count_nonzero(model)(0,0)));
     double countP = (static_cast<double>(nc::count_nonzero(probe)(0,0)));
+
+    // Computes the distance
     double distance = 1. - countMP / (countM + countP);
     return distance;
 }
--- alignment-cpp/distance.hpp
+++ alignment-cpp/distance.hpp
@@ -3,6 +3,15 @@
 #ifndef DISTANCE_H
 #define DISTANCE_H
 
+/**
+ * This function computes the distance between two extracted fingervein images.
+ * 
+ * @param[in] model: A 2-dimensional NdArray of bools which we 
+ * compare to another fingervein image.
+ * @param[in] probe: A 2-dimensional NdArray of bools which we 
+ * compare to the model to compute a distance value.
+ * @returns A double denoting the distance between the two input images.
+*/
 double compute_miura_distance (nc::NdArray<bool> model, nc::NdArray<bool> probe);
 
 #endif
